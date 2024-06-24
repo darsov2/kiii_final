274ed9709bb7d92efb24b22e418b5a17a12cd28f
darsov2 <62809499+darsov2@users.noreply.github.com>
2024-06-24 23:34:46 +0200

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import jsonpAdapter from 'axios-jsonp';
 const instance = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "http://backend-service:8080/",
     headers: { 
         // "Access-Control-Allow-Origin": "*"
     }
