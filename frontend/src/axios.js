9b1908bd7d55c935d4c98a7f93eaf7ca2632f2da
darsov2 <62809499+darsov2@users.noreply.github.com>
2024-06-26 18:12:22 +0200

    frontend url change

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import jsonpAdapter from 'axios-jsonp';
 const instance = axios.create({
-    baseURL: "http://localhost/",
+    baseURL: "http://backend.com/",
     headers: { 
         // "Access-Control-Allow-Origin": "*"
     }
