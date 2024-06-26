956e65ae0214bce0178caefb36391ed31fdd98df
darsov2 <62809499+darsov2@users.noreply.github.com>
2024-06-26 18:20:02 +0200

    cors url adjustment

--- backend/api-gateway/src/main/java/mk/ukim/finki/apigateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/mk/ukim/finki/apigateway/ApiGatewayApplication.java
@@ -39,7 +39,7 @@ public class ApiGatewayApplication {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins("http://frontend.com")
                     .allowedMethods("GET", "POST", "PUT", "DELETE");
         }
     }
